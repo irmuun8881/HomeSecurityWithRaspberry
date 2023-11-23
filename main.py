d442f672a19a6304fd05fe68445db2540ce56e4e
irmuun8881 <irmuun8881@gmail.com>
2023-11-23 16:07:03 +0900

    Added image_folder, updated main code.

--- main.py
+++ main.py
@@ -1,63 +1,106 @@
 import cv2
 import smtplib
-import imghdr
 from email.message import EmailMessage
+import numpy as np
+import dlib
+import datetime
+import os
 import face_recognition
+from dotenv import load_dotenv
 
-cap = cv2.VideoCapture(0)
-pedestrian_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
+load_dotenv()
+# Function to get face encodings from known images
+def get_face_encodings(image_folder):
+    known_face_encodings = []
+    known_face_names = []
 
-threshold_area = 5000
-notification_sent = False
+    for image_name in os.listdir(image_folder):
+        if image_name.endswith('.jpg') or image_name.endswith('.png'):
+            image_path = os.path.join(image_folder, image_name)
+            face_image = face_recognition.load_image_file(image_path)
+            face_encodings = face_recognition.face_encodings(face_image)
 
-sender_email = "your_email@gmail.com"
-sender_password = "your_password"
-receiver_email = "recipient_email@example.com"
+            if face_encodings:
+                known_face_encodings.append(face_encodings[0])
+                known_face_names.append(os.path.splitext(image_name)[0])
 
-def send_email_notification():
+    return known_face_encodings, known_face_names
+
+# Initialize dlib's face detector and face recognition model using environment variables
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor(os.getenv('SHAPE_PREDICTOR_PATH'))
+facerec = dlib.face_recognition_model_v1(os.getenv('FACE_RECOGNITION_MODEL_PATH'))
+
+# Initialize video capture
+video_capture = cv2.VideoCapture(0)
+
+# Load known face encodings and names
+images_folder = "/Users/JessFort/Documents/My_Coding_folder/IOT_project/image_folder"  # Update with the path to your images folder
+known_face_encodings, known_face_names = get_face_encodings(images_folder)
+
+# Email sending cooldown setup
+email_sent_time = None
+cooldown_minutes = 5
+
+# Email credentials from environment variables
+sender_email = os.getenv('SENDER_EMAIL')
+sender_password = os.getenv('SENDER_PASSWORD')
+receiver_email = os.getenv('RECEIVER_EMAIL')
+
+def send_email_notification(image_path):
+    # Make sure the contents of this function are indented
     msg = EmailMessage()
-    msg['Subject'] = 'Person Detected at the Door'
+    msg['Subject'] = 'Unrecognized Person Detected'
     msg['From'] = sender_email
     msg['To'] = receiver_email
-    msg.set_content('A person has been detected at the door!')
+    msg.set_content('An unrecognized person has been detected at the door.')
 
-    with open('person_detected.jpg', 'rb') as f:
-        file_data = f.read()
-        file_type = imghdr.what(f.name)
-        file_name = f.name
+    with open(image_path, 'rb') as img:
+        file_data = img.read()
+        file_type = 'jpg'
+        file_name = image_path
 
     msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(sender_email, sender_password)
         smtp.send_message(msg)
-
-known_family_members = ["Alice", "Bob", "Charlie"]  # Example family members
-
 while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    pedestrians = pedestrian_cascade.detectMultiScale(gray, 1.3, 5)
+    ret, frame = video_capture.read()
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    for (x, y, w, h) in pedestrians:
-        if x < 400:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # Detect faces
+    faces = detector(rgb_frame)
+    face_encodings = []
 
-            if not notification_sent:
-                cv2.imwrite('person_detected.jpg', frame)
-                detected_person = "Unknown"
+    for face in faces:
+        shape = sp(rgb_frame, face)
+        face_encoding = np.array(facerec.compute_face_descriptor(rgb_frame, shape))
+        face_encodings.append(face_encoding)
 
-                # Face recognition logic
-                unknown_face_encoding = face_recognition.face_encodings(frame)[0]
-                results = face_recognition.compare_faces(known_encodings, unknown_face_encoding)
-                if not any(results):  # If no match found
-                    send_email_notification()
-                    notification_sent = True
+    face_names = []
+    for face_encoding in face_encodings:
+        # Compare face encodings with known faces
+        if known_face_encodings:
+            distances = np.linalg.norm(known_face_encodings - face_encoding, axis=1)
+            match = np.any(distances <= 0.6)
+            name = "Unknown"
+            if match:
+                first_match_index = np.argmin(distances)
+                name = known_face_names[first_match_index]
+        else:
+            name = "Unknown"
+        face_names.append(name)
 
-    cv2.imshow('Person Detection', frame)
+    if "Unknown" in face_names:
+        current_time = datetime.datetime.now()
+        if email_sent_time is None or (current_time - email_sent_time).total_seconds() > cooldown_minutes * 60:
+            cv2.imwrite('unrecognized_person.jpg', frame)
+            send_email_notification('unrecognized_person.jpg')
+            email_sent_time = current_time
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
+video_capture.release()
 cv2.destroyAllWindows()
